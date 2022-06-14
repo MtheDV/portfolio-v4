fda45a6c306e68fba06d8a1ecf8c21ba4ee37980
Mathew de Vin <mathewdevin03@gmail.com>
2022-06-14 15:23:37 -0700

    Add background option to sticky container
     - Add boolean value to change background color

--- components/Effects/StickyContainer.js
+++ components/Effects/StickyContainer.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import stickyContainerStyles from '../../styles/Effects/StickyContainer.module.scss';
 
-const StickyContainer = ({children}) => {
+const StickyContainer = ({hasBackground, children}) => {
   return (
     <div className={stickyContainerStyles.sticky_container}>
       <div className={stickyContainerStyles.sticky_container__top}>
         <div className={stickyContainerStyles.sticky_container__top_whitespace}></div>
         <div className={stickyContainerStyles.sticky_container__top_border}></div>
       </div>
-      <div className={stickyContainerStyles.sticky_container__content}>
+      <div className={`${stickyContainerStyles.sticky_container__content} ${hasBackground && stickyContainerStyles.sticky_container__content_background}`}>
         {children}
       </div>
       <div className={stickyContainerStyles.sticky_container__bottom}>
